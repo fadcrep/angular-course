2d83bc5625912f9e7e77a799c8e673576d526984
Crépin Hugues FADJO <fadcrepin@gmail.com>
2021-10-19 07:56:48 +0000

    content tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, QueryList, ViewChildren } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
 
 @Component({
@@ -7,11 +8,19 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  
 
   courses:Course[]= COURSES;
   startDate = new Date(2000,0,1);
 
+  @ViewChildren(CourseCardComponent)
+  cards: QueryList<CourseCardComponent>;
+
+  ngAfterViewInit(){
+  
+    console.log(this.cards);
+  }
 
   onCourseSelected(course:Course){
     console.log("App component- click event bubbled ...", course);
